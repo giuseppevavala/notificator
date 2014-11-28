c65f8a93dae035077afbbc975c347306d092f1c2
Giuseppe <giuse.vavala@gmail.com>
2014-11-28 13:53:01 +0100

    Icona bus

--- demone.py
+++ demone.py
@@ -3,9 +3,17 @@ from check import orariFermata
 import time
 
 
-ris = orariFermata(1724)
+
 while True:
-	for el in ris:
-		if (el['orario'][0] < 10):
-			sendmessage ("Mancano %s minuti per il %s" % (str(el['orario'][0]), str(el['linea'])), "Pullman")
+	try:
+		ris = orariFermata(1724)
+		for el in ris:
+			if ((el['orario'][0] < 12) and (el['orario'][0] >= 10)):
+				sendmessage ("Mancano %s minuti per il %s" % (str(el['orario'][0]), str(el['linea'])), "Pullman", 'busv')
+			elif ((el['orario'][0] < 6) and (el['orario'][0] >= 4)):
+				sendmessage ("Mancano %s minuti per il %s" % (str(el['orario'][0]), str(el['linea'])), "Pullman", 'busr')
+		print ris	
+	except Exception:
+		print "Eccezione"
 	time.sleep (60)
+	
--- notificator.py
+++ notificator.py
@@ -3,10 +3,17 @@
 
 import subprocess
 import platform
+import os
 
-def sendmessage(message, title):
-	if (platform.system() == 'Linux'):
-		subprocess.Popen(['notify-send', title, message, '--icon=dialog-information'])
-	elif (platform.system() == 'Darwin'):
-		subprocess.Popen(['terminal-notifier', "-message", message, "-title", title])
+def sendmessage(message, title, icona):
+	if (icona == 'busv'):
+		if (platform.system() == 'Linux'):
+			subprocess.Popen(['notify-send', title, message, '--icon=%s/bus.png' % (os.getcwd())])
+		elif (platform.system() == 'Darwin'):
+			subprocess.Popen(['terminal-notifier', "-message", message, "-title", title])
+	elif (icona == 'busr'):
+		if (platform.system() == 'Linux'):
+			subprocess.Popen(['notify-send', title, message, '--icon=%s/busrosso.png' % (os.getcwd())])
+		elif (platform.system() == 'Darwin'):
+			subprocess.Popen(['terminal-notifier', "-message", message, "-title", title])
 
